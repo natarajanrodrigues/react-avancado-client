d79f585a5b1bf9a62abee30b60de2cad38f675c2
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2020-11-29 18:46:15 -0300

    trying add prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head"
 import styles from '../styles/Home.module.css'
 
 type Props = {
-  title: string
+  title: string;
 }
 
 export default function Home({ title = 'React Avançado'}: Props) {
